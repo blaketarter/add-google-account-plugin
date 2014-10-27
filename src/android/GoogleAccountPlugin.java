6526891eef0b912bc7277a4e6a37aed54bdb0d23
Blake Tarter <blake@dhcp-2-133.kan-mo.intomni.com>
2014-10-27 14:33:39 -0500

    updating to start on forceAppUpdate development and some code refactoring

--- src/android/GoogleAccountPlugin.java
+++ src/android/GoogleAccountPlugin.java
@@ -1,10 +1,9 @@
 package com.btarter.GoogleAccount;
 
 import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,6 +15,12 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
 import android.util.Patterns;
+import android.net.Uri;
+import android.text.Html;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 
 /**
  * The Class GoogleAccountPlugin.
@@ -60,7 +65,64 @@ public class GoogleAccountPlugin extends CordovaPlugin {
           } catch (Error e) {
             return false;
           }
-        }
+        } else if (action.equals("forceAppUpdate")) {
+          // Parse the arguments
+          JSONObject obj = args.getJSONObject(0);
+          String type = obj.has("type") ? obj.getString("type") : null;
+          Uri uri = obj.has("url") ? Uri.parse(obj.getString("url")) : null;
+          JSONObject extras = obj.has("extras") ? obj.getJSONObject("extras") : null;
+          Map<String, String> extrasMap = new HashMap<String, String>();
+
+          // Populate the extras if any exist
+          if (extras != null) {
+              JSONArray extraNames = extras.names();
+              for (int i = 0; i < extraNames.length(); i++) {
+                  String key = extraNames.getString(i);
+                  String value = extras.getString(key);
+                  extrasMap.put(key, value);
+              }
+          }
+
+          try {
+            forceActivity(obj.getString("action"), uri, type, extrasMap);
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
+            return true;
+          } catch (Error e) {
+            return false;
+          }
         return false;
+    }
+
+    void forceActivity(String action, Uri uri, String type, Map<String, String> extras) {
+        Intent i = (uri != null ? new Intent(action, uri) : new Intent(action));
+
+        if (type != null && uri != null) {
+            i.setDataAndType(uri, type); //Fix the crash problem with android 2.3.6
+        } else {
+            if (type != null) {
+                i.setType(type);
+            }
+        }
+
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        for (String key : extras.keySet()) {
+            String value = extras.get(key);
+            // If type is text html, the extra text must sent as HTML
+            if (key.equals(Intent.EXTRA_TEXT) && type.equals("text/html")) {
+                i.putExtra(key, Html.fromHtml(value));
+            } else if (key.equals(Intent.EXTRA_STREAM)) {
+                // allowes sharing of images as attachments.
+                // value in this case should be a URI of a file
+                i.putExtra(key, Uri.parse(value));
+            } else if (key.equals(Intent.EXTRA_EMAIL)) {
+                // allows to add the email address of the receiver
+                i.putExtra(Intent.EXTRA_EMAIL, new String[] { value });
+            } else {
+                i.putExtra(key, value);
+            }
+        }
+        
+        this.cordova.getActivity().startActivity(i);
     }
 }
--- www/GoogleAccount.js
+++ www/GoogleAccount.js
@@ -7,16 +7,24 @@
 /**
  * @return the google account class instance
  */
- var GoogleAccount = {
+CK = ( typeof CK == 'undefined' ? {} : CK );
+var cordova = window.cordova || window.Cordova;
 
-   addGoogleAccount: function(successCallback, failureCallback){
-     cordova.exec(successCallback, failureCallback, 'GoogleAccountPlugin',
-       'addGoogleAccount', []);
-   },
-   getGoogleAccount: function(successCallback, failureCallback){
-     cordova.exec(successCallback, failureCallback, 'GoogleAccountPlugin',
-       'getGoogleAccount', []);
-   }
- };
+CK.app = {
 
- module.exports = GoogleAccount;
+  addGoogleAccount: function(successCallback, failureCallback){
+   return cordova.exec(successCallback, failureCallback, 'GoogleAccountPlugin',
+     'addGoogleAccount', []);
+  },
+  getGoogleAccount: function(successCallback, failureCallback){
+   return cordova.exec(successCallback, failureCallback, 'GoogleAccountPlugin',
+     'getGoogleAccount', []);
+  },
+  startActivity : function(params, success, fail) {
+    return cordova.exec(function(args) {
+        success(args);
+    }, function(args) {
+        fail(args);
+    }, 'GoogleAccountPlugin', 'forceAppUpdate', [params]);
+  }
+};
